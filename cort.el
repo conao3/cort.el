10a8a5240a8e81ba1cdaa6bbefd8591e8c722a98
Naoya Yamashita <conao3@gmail.com>
2018-12-08 00:37:03 +0900

    tagged v3.0.6

--- cort.el
+++ cort.el
@@ -31,7 +31,7 @@
   "Simplify elisp test framework."
   :group 'lisp)
 
-(defconst cort-version "3.0.5"
+(defconst cort-version "3.0.6"
   "cort.el version")
 
 (defconst cort-env-symbols '(:cort-emacs<
