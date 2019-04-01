c3ff3b3e6b8ade342257a45f148307e4f55d69f7
conao3 <conao3@gmail.com>
2019-04-01 11:01:00 +0900

    tagged v5.0.2

--- cort-test.el
+++ cort-test.el
@@ -35,7 +35,7 @@
   "Simplify elisp test framework."
   :group 'lisp)
 
-(defconst cort-version "5.0.0"
+(defconst cort-version "5.0.2"
   "cort.el version")
 
 (defconst cort-env-symbols '(:cort-emacs<
