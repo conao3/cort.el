55cae9f67b376cf0ff9042a877c5474bdd8cec74
conao3 <conao3@gmail.com>
2019-04-30 00:20:02 +0900

    update license and copyright

--- cort-tests.el
+++ cort-tests.el
@@ -1,32 +1,37 @@
-;;; cort-tests.el ---                                 -*- lexical-binding: t; -*-
+;;; cort-tests.el --- cort-test test file       -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2018  Naoya Yamashita
+;; Copyright (C) 2018-2019 Naoya Yamashita <conao3@gmail.com>
 
 ;; Author: Naoya Yamashita <conao3@gmail.com>
 ;; Maintainer: Naoya Yamashita <conao3@gmail.com>
 ;; Keywords: test lisp
-;; Version: 4.0.0
+;; Version: 6.0.0
 ;; URL: https://github.com/conao3/cort.el
-;; Package-Requires: ((emacs "22.0"))
+;; Package-Requires: ((emacs "24.0"))
 
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
+;;   Abobe declared this package requires Emacs-24, but it's for warning
+;;   suppression, and will actually work from Emacs-22.
 
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the Affero GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the Affero
 ;; GNU General Public License for more details.
 
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; You should have received a copy of the Affero GNU General Public
+;; License along with this program.  If not, see
+;; <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
-;;
+;; cort-test test file
 
 ;;; Code:
+
 (require 'cort-test)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
