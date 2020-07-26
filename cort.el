b751ff03da85a1767487d839b83b39320a2ca4bf
conao3 <conao3@gmail.com>
2020-07-26 12:25:37 +0900

    tagged v7.1.4

--- cort.el
+++ cort.el
@@ -5,7 +5,7 @@
 ;; Author: Naoya Yamashita <conao3@gmail.com>
 ;; Maintainer: Naoya Yamashita <conao3@gmail.com>
 ;; Keywords: test lisp
-;; Version: 7.1.3
+;; Version: 7.1.4
 ;; URL: https://github.com/conao3/cort.el
 ;; Package-Requires: ((emacs "24.1") (ansi "0.4") (cl-lib "0.6"))
 
